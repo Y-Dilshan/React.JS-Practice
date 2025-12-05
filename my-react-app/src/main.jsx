98917198499b30e31af3806e53b132373fac3141
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2025-12-05 20:26:59 +0530

    I written some react functions to practice

---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,62 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+/* function Car(){
+  return(
+    <h1 className='myClass'>Hello </h1>
+  );
+}
+createRoot(document.getElementById('root')).render(
+  <Car />
+);*/
+
+/* function Car(){
+  const myfunc=()=>{
+    alert('Hello World!........')
+  }
+  return(
+    <button onClick={myfunc}>Click Me!</button>
+  );
+}
+createRoot(document.getElementById('root')).render(
+  <Car/>
+); */
+
+/* function Car(){
+  const myfunc=()=>{
+    alert('Hello World!........')
+  }
+  return(
+    <button onClick={myfunc} disabled>Click Me!</button>
+  );
+}
+createRoot(document.getElementById('root')).render(
+  <Car/>
+); */
+
+/* function Car(){
+  const myfunc=()=>{
+    alert('Hello World!........')
+  }
+  return(
+    <button onClick={myfunc} disabled={true}>Click Me!</button>
+  );
+}
+createRoot(document.getElementById('root')).render(
+  <Car/>
+); */
+
+/* function Car(){
+  const myfunc=()=>{
+    alert('Hello World!........')
+  }
+  return(
+    <button onClick={myfunc} disabled={false}>Click Me!</button>
+  );
+}
+createRoot(document.getElementById('root')).render(
+  <Car/>
+); */
+
